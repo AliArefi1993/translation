7569ee4f5d2cc41256b024e3c2c3d983d08e68fe
Ali Arefi <ali.are@mtnirancell.com>
2024-11-24 16:28:47 +0330

    modify model path

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ import os
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
 RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT", "5672"))
 
-TRANSLATION_MODEL_PATH = "./app/models/translate-en_fa-1_5.argosmodel"
+TRANSLATION_MODEL_PATH = "/app/app/models/translate-en_fa-1_5.argosmodel"
 
 ASR_QUEUE = "asr_task_queue"
 TRANSLATION_QUEUE = "translation_task_queue"
